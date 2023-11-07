6002e4390f3b2e5b58c176cffdd2d86e20ce4222
hasnatjamil <hasnat.jamil@enosisbd.com>
2023-11-07 12:13:49 +0600

    changeset: add prettier and update the style rules

--- src/test/steps/login.step.ts
+++ src/test/steps/login.step.ts
@@ -1,9 +1,9 @@
-import { Given } from "@cucumber/cucumber";
+import { Given } from '@cucumber/cucumber'
 
-Given("User navigates to the application", async function () {
-  console.log("implementation done");
-});
+Given('User navigates to the application', async function () {
+  console.log('implementation done')
+})
 
-Given("User click on the login link", () => {
+Given('User click on the login link', () => {
   // Write code here that turns the phrase above into concrete actions
-});
+})
