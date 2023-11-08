f41edbe1189ced03b2293bdb9d72d7e61ccf96bf
hasnatjamil <hasnat.jamil@enosisbd.com>
2023-11-08 12:57:07 +0600

    changset: setup eslint

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -5,13 +5,13 @@ Feature: User authentication validations
     And User click on the login link
 
   Scenario: User should be able to login for valid credentials
-    Given User enters the username 'sample-user'
-    And User enters the password 'secret-sauce'
+    Given User enters the username as 'sample-user'
+    And User enters the password as 'secret-sauce'
     When User clicks on the login button
     Then Login should be successful
 
   Scenario: User should not be able to login for invalid credentials
-    Given User enters the username 'sample-user'
+    Given User enters the username as 'sample-user'
     And User enters the password 'not-secret-sauce'
     When User clicks on the login button
     But Login should be unsuccessful
--- src/test/steps/login.step.ts
+++ src/test/steps/login.step.ts
@@ -1,9 +1,41 @@
-import { Given } from '@cucumber/cucumber'
+import { Given, Then, When } from '@cucumber/cucumber'
 
 Given('User navigates to the application', async function () {
   console.log('implementation done')
 })
 
 Given('User click on the login link', () => {
+  console.log('implementation pending')
+})
+
+Given('User enters the username {string}', (s: string) => {
+  console.log('implementation pending')
+})
+
+When('Login should be unsuccessful', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('User clicks on the login button', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Given('User enters the password {string}', (s: string) => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Then('Login should be successful', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('User clicks on the login button', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Given('User enters the password as {string}', (s: string) => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Given('User enters the username as {string}', (s: string) => {
   // Write code here that turns the phrase above into concrete actions
 })
